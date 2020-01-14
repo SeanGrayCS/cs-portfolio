62feb802c62b7ac4d7150ac0afec6950aa24e363
Sean Gray <Gray_902073@leanderisd.org>
2020-01-13 20:18:08 -0600

    Adjusted Array Index to be more readable and use overloading instead of a switch.

--- Personal Projects/Array Index/ArrayIndex.java
+++ Personal Projects/Array Index/ArrayIndex.java
@@ -18,157 +18,124 @@ public class ArrayIndex {
 	private boolean[] arrBoolean;
 	private char[] arrChar;
 	private String[] arrString;
-	
-	private int dataType;
-	
-	public ArrayIndex() {
-		dataType = -1;
-	}
+		
     public ArrayIndex(int[] arr) {
     	arrInt = arr;
-    	dataType = 0;
     }
     public ArrayIndex(long[] arr) {
     	arrLong = arr;
-    	dataType =  1;
     }
     public ArrayIndex(short[] arr) {
     	arrShort = arr;
-    	dataType = 2;
     }
     public ArrayIndex(double[] arr) {
     	arrDouble = arr;
-    	dataType = 3;
     }
     public ArrayIndex(float[] arr) {
     	arrFloat = arr;
-    	dataType = 4;
     }
     public ArrayIndex(byte[] arr) {
     	arrByte = arr;
-    	dataType = 5;
     }
     public ArrayIndex(boolean[] arr) {
     	arrBoolean = arr;
-    	dataType = 6;
     }
     public ArrayIndex(char[] arr) {
     	arrChar = arr;
-    	dataType = 7;
     }
     public ArrayIndex(String[] arr) {
     	arrString = arr;
-    	dataType = 8;
     }
     
-    public int indexOf(Object in)
-    {
-    	if(dataType == -1)
+    public int indexOf(int in) {
+		for(int i = 0; i<arrInt.length; i++)
     	{
-    		return -1;
+    		if(arrInt[i] == in)
+    		{
+    			return i;
+    		}
     	}
-		if(dataType == 0)
-		{
-			for(int i = 0; i<arrInt.length; i++)
+    	return -1;
+	}
+	public int indexOf(long in) {
+		for(int i = 0; i<arrLong.length; i++)
+    	{
+    		if(arrLong[i] == in)
     		{
-    			if(arrInt[i] == (int) in)
-    			{
-    				return i;
-    			}
+    			return i;
     		}
-    		return -1;
-		}
-		if(dataType == 1)
-		{
-			for(int i = 0; i<arrLong.length; i++)
+    	}
+    	return -1;
+	}
+	public int indexOf(short in) {
+		for(int i = 0; i<arrShort.length; i++)
+    	{
+    		if(arrShort[i] == in)
     		{
-    			if(arrLong[i] == (long) in)
-    			{
-    				return i;
-    			}
+    			return i;
     		}
-    		return -1;
-		}
-		if(dataType == 2)
-		{
-			for(int i = 0; i<arrShort.length; i++)
+    	}
+    	return -1;
+	}
+	public int indexOf(double in) {
+		for(int i = 0; i<arrDouble.length; i++)
+    	{
+    		if(arrDouble[i] == in)
     		{
-    			if(arrShort[i] == (short) in)
-    			{
-    				return i;
-    			}
+    			return i;
     		}
-    		return -1;
-		}
-		if(dataType == 3)
-		{
-			for(int i = 0; i<arrDouble.length; i++)
+    	}
+    	return -1;
+	}
+	public int indexOf(float in) {
+		for(int i = 0; i<arrFloat.length; i++)
+    	{
+    		if(arrFloat[i] == in)
     		{
-    			if(arrDouble[i] == (double) in)
-    			{
-    				return i;
-    			}
+    			return i;
     		}
-    		return -1;
-		}
-		if(dataType == 4)
-		{
-			for(int i = 0; i<arrFloat.length; i++)
+    	}
+    	return -1;
+	}
+	public int indexOf(byte in) {
+		for(int i = 0; i<arrByte.length; i++)
+    	{
+    		if(arrByte[i] == in)
     		{
-    			if(arrFloat[i] == (float) in)
-    			{
-    				return i;
-    			}
+    			return i;
     		}
-    		return -1;
-		}
-		if(dataType == 5)
-		{
-			for(int i = 0; i<arrByte.length; i++)
+    	}
+    	return -1;
+	}
+	public int indexOf(boolean in) {
+		for(int i = 0; i<arrBoolean.length; i++)
+    	{
+    		if(arrBoolean[i] == in)
     		{
-    			if(arrByte[i] == (byte) in)
-    			{
-    				return i;
-    			}
+    			return i;
     		}
-    		return -1;
-		}
-		if(dataType == 6)
-		{
-			for(int i = 0; i<arrBoolean.length; i++)
+    	}
+    	return -1;
+	}
+	public int indexOf(char in) {
+		for(int i = 0; i<arrChar.length; i++)
+    	{
+    		if(arrChar[i] == in)
     		{
-    			if(arrBoolean[i] == (boolean) in)
-    			{
-    				return i;
-    			}
+    			return i;
     		}
-    		return -1;
-		}
-		if(dataType == 7)
-		{
-			for(int i = 0; i<arrChar.length; i++)
+    	}
+    	return -1;
+	}
+    public int indexOf(String in) {
+		for(int i = 0; i<arrString.length; i++)
+    	{
+    		if(arrString[i] == in)
     		{
-    			if(arrChar[i] == (char) in)
-    			{
-    				return i;
-    			}
+    			return i;
     		}
-    		return -1;
-		}
-		if(dataType == 8)
-		{
-			for(int i = 0; i<arrString.length; i++)
-    		{
-    			if(arrString[i] == (String) in)
-    			{
-    				return i;
-    			}
-    		}
-    		return -1;
-		}
-		return -1;
-			
-    }
-    
+    	}
+    	return -1;
+	}
     
 }
