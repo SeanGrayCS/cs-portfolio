45b4f8ee8bf546257dd7b1268acfe35777c7c9ed
Sean Gray <sean.gray@family-gray.net>
2020-02-06 12:21:58 -0600

    Finished newest labs.

--- CS2 Labs/Semester2/4thSixWeeks/lab16d-OddToEven/ListOddToEven.java
+++ CS2 Labs/Semester2/4thSixWeeks/lab16d-OddToEven/ListOddToEven.java
@@ -6,12 +6,34 @@
  * @author - Id 902073
  */
 
+import java.util.List;
 import java.util.ArrayList;
 
 public class ListOddToEven
 {
-	public static int go( ArrayList<Integer> ray )
+	public static int go(List<Integer> ray)
 	{
-		return false;
+		ArrayList<Integer> rayList = new ArrayList<Integer>(); //put List ray into an ArrayList
+		rayList.addAll(ray);
+		int oddIndex = 0; //instantiate variables
+		int evenIndex = 0;
+		boolean hasOdd = false;
+		boolean hasEven = false;
+		for (int i = 0; i < rayList.size(); i++)
+		{
+			if (rayList.get(i) % 2 == 1 && !hasOdd) //find the first odd
+			{
+				oddIndex = i;
+				hasOdd = true;
+			}
+			if (rayList.get(i) % 2 == 0 && hasOdd) //write over each even after the first odd with the next even
+			{
+				evenIndex = i;
+				hasEven = true;
+			}
+		}
+		if (!hasOdd || !hasEven) //return -1 if there were no odds or no evens after odds
+			return -1;
+		return evenIndex - oddIndex; //return the distance
 	}
 }
