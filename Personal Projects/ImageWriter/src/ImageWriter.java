dfb5bd96fc3d08b00d7c95fcf744a61ad3018fe4
SeanGrayCS <59578181+SeanGrayCS@users.noreply.github.com>
2020-02-22 22:31:40 -0600

    Added files needed for ImageWriter and SoundPlayer.

--- Personal Projects/ImageWriter/src/ImageWriter.java
+++ Personal Projects/ImageWriter/src/ImageWriter.java
@@ -1,5 +1,5 @@
 /**
- * @MyImage.java
+ * @ImageWriter.java
  *
  * @author Sean Gray
  *
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
-public class MyImage {
+public class ImageWriter {
 
     public static void main(String[] args) throws IOException {
     	//int[][][] pixels = new int[1080][1920][4];
@@ -19,7 +19,7 @@ public class MyImage {
     	File f = null;
 		
 		try {
-			f = new File("C:\\Users\\gray_902073\\Desktop\\Image.jpg");
+			f = new File("Image.jpg");
 			image = ImageIO.read(f);
 		} catch(IOException e) {
 			System.out.println("Error: "+e);
@@ -37,7 +37,7 @@ public class MyImage {
 		}
 		
 		try{
-    	  	f = new File("C:\\Users\\gray_902073\\Desktop\\Output.jpg");
+    	  	f = new File("Output.jpg");
   	 		ImageIO.write(image, "jpg", f);
      		System.out.println("Writing complete.");
     	} catch(IOException e){
